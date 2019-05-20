48da74a44a90be167b2c4a0d4c191cda8576d558
khaled samer hassan elsedek <kh.elsedek@gmail.com>
2019-05-20 06:26:20 +0200

    Add files via upload

---
+++ xyz-acceolemeter/xyz-acceolemeter/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  xyz-acceolemeter
+//
+//  Created by Khaled on 5/4/19.
+//  Copyright © 2019 Khaled. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+    }
+    func updateLabels(data:CMAccelerometerData?, error: Error?)  {
+        guard let accelerometerData = data else { return}
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        let x = formatter.string(for: accelerometerData.acceleration.x)!
+        let y = formatter.string(for: accelerometerData.acceleration.y)!
+        let z = formatter.string(for: accelerometerData.acceleration.z)!
+        xLabel.text = "x: \(x)"
+        yLabel.text = "y: \(y)"
+        zLabel.text = "z: \(z)"
+        
+        
+    }
+}
+
